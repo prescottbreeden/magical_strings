8dd5c2c323809c9dce7f775812b20abd523e5f01
prescottbreeden <prescott.breeden@gmail.com>
2019-11-15 22:43:11 -0800

    minor change to contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Contact = props => {
-  return <React.Fragment>Contacts Coming Soon...</React.Fragment>;
+  return (
+    <React.Fragment>
+      <h2 className="event__title">Contact Us</h2>
+    </React.Fragment>
+  );
 };
 
 export default Contact;
