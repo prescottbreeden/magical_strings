fec74eca67da06d5777d9fa15edf40915a7d7d52
prescottbreeden <prescott.breeden@gmail.com>
2020-10-31 21:38:33 -0700

    updated react-scripts and about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,12 +10,14 @@ const About = props => {
           the joy of Celtic music and dance in the lives of people around the
           United States and abroad since 1978. They have recorded 21 albums
           encompassing traditional Celtic arrangements, original compositions
-          and subtle world music influences, collaborating with incredible
-          musicians such as Dan Fogelberg, Garrison Keillor, Alex di Grassi,
-          Amadou Jobarteh, and Keola Beamer. When not on the road, Philip and
-          Pam teach, build instruments, compose, and seem to have boundless
-          energy for their creative life which is nurtured at their home, Magic
-          Hill, on a beautiful farm by the Puget Sound.
+          and subtle world music influences, performing on their own crafted
+          Celtic harps and hammered dulcimers, augmented with pennywhistles,
+          button accordion, concertina and more. They have toured and
+          collaborated with notable artists such as singer/songwriter Dan
+          Fogelberg and slack-key guitarist Keola Beamer. When not on the road,
+          Philip and Pam teach, build instruments, compose, and seem to have
+          boundless energy for their creative life which is nurtured at their
+          home, Magic Hill, on a beautiful farm by the Puget Sound.
         </p>
       </section>
     </React.Fragment>
